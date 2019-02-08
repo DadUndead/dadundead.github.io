e3edabbb838f6db924d44cb522b2b0d5dd6e1e4d
Aleksey Klimenko <ralict@mail.ru>
2019-02-08 06:42:40 +0300

    initial commit:
    - basic layout
    - menu
    - particles
    - deploy

---
+++ precache-manifest.d2f1bc7f00a48efd24c6ec26d579cb87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0e594144f6569f9a9d4683ebb9f2da7",
+    "url": "/static/media/overlay.b0e59414.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a26701432585bd8676a",
+    "url": "/static/js/main.8a267014.chunk.js"
+  },
+  {
+    "revision": "f9e5aa23c595bb7d8484",
+    "url": "/static/js/1.f9e5aa23.chunk.js"
+  },
+  {
+    "revision": "8a26701432585bd8676a",
+    "url": "/static/css/main.ee15d234.chunk.css"
+  },
+  {
+    "revision": "55a78c6e50977623752cb8ed04e58b8b",
+    "url": "/index.html"
+  }
+];
